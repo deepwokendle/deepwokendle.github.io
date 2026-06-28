2ea7dbc678a1dbf2fb62971f0783dfcd88d708ec
Matheus <angelblast220@gmail.com>
2026-06-27 23:58:50 -0300

    feat: updates year

--- docs/src/components/common/Footer.tsx
+++ docs/src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>&copy; 2025 Deepwokendle</span> |
+      <span>&copy; 2026 Deepwokendle</span> |
       <Link className="underscoreAnimation privacyLink" to="/privacy">
         Privacy Policy
       </Link>
